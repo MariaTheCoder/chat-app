8b697bd8d92a49a53b7ebc0e5166fba9f90d331e
Maria T. Andersen <mteilmann93@gmail.com>
2022-07-16 18:07:44 +0200

    react to getting poked

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ function App() {
   const socket = useRef(io("http://localhost:3001"));
 
   useEffect(() => {
+    console.log(socket.current);
     socket.current.on("chat message", (msg) => {
+      console.log(msg);
+    });
+
+    socket.current.on("poke", (msg) => {
       console.log(msg);
     });
   }, []);
