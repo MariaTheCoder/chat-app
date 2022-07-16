dbe20fd3df3ee7cf7e838ebacc502cbfa4f6db19
Maria T. Andersen <mteilmann93@gmail.com>
2022-07-16 17:51:04 +0200

    initial code to establish and check WS connection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { io } from "socket.io-client";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    if (!socket) {
+      const newSocket = io("http://localhost:3001");
+      console.log(newSocket);
+      newSocket.emit("chat message", "Hello from React");
+      newSocket.on("chat message", (msg) => {
+        console.log(msg);
+      });
+      setSocket(newSocket);
+    }
+  }, [socket]);
+
   return (
     <div className="App">
       <header className="App-header">
